ba6732aeb6f6b3dc9eacb01dfdb3efd5c9ef798c
linjing <linjingljlj@gmail.com>
2014-03-25 09:35:50 +0800

    fix

--- sorter/sorter/algorithms/bubblesort/bubblesort.go
+++ sorter/sorter/algorithms/bubblesort/bubblesort.go
@@ -3,9 +3,9 @@ package bubblesort
 func BubbleSort(values [] int) {
   flag := true
   
-  for i := 0; i < len(values) - 1; ++i {
+  for i := 0; i < len(values) - 1; i = i + 1 {
     flag = true
-    for j := 0; j <len(values) - i - 1; ++j {
+    for j := 0; j <len(values) - i - 1; j = j + 1 {
       if values[j] > values[j + 1] {
         values[j], values[j + 1] = values[j + 1], values[j]
         flag = false
